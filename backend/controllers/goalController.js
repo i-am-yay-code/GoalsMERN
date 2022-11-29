b69bfd08411100a7d87fd41f8d1986aa4f6e97d1
Anton Yahushchyn <i.am.yay.code@gmail.com>
2022-11-29 23:23:48 +0200

    First part of the tutorial

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,11 +1,16 @@
 const asyncHandler = require('express-async-handler');
+const { request } = require('http');
+
+const Goal = require('../models/goalModel')
 
 
 // @desc Get goals
 // @route GET /api/goals
 // @access Priate
 const getGoals = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: "Get goals" });
+    const goals = await Goal.find()
+
+    res.status(200).json({ message: goals });
 })
 
 // @desc Set goal
@@ -17,7 +22,12 @@ const setGoal = asyncHandler(async (req, res) => {
         res.status(400)
         throw new Error("Please, set a text field")
     }
-    res.status(200).json({ message: "Set goal" })
+
+    const goal = await Goal.create({
+        text: req.body.text
+    })
+
+    res.status(200).json({ message: goal })
 })
 
 
@@ -25,7 +35,17 @@ const setGoal = asyncHandler(async (req, res) => {
 // @route PUT /api/goals
 // @access Priate
 const updateGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Update goal ${req.params.id}` })
+
+    const goal = await Goal.findById(req.params.id)
+
+    if (!goal) {
+        res.status(400);
+        throw new Error("Goal not found")
+    }
+
+    const updatedGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, { new: true })
+
+    res.status(200).json({ message: `Updated goal ${updatedGoal}` })
 })
 
 
@@ -33,7 +53,18 @@ const updateGoal = asyncHandler(async (req, res) => {
 // @route DELETE /api/goals
 // @access Priate
 const deleteGoal = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: `Delete ${req.params.id}` })
+
+    const goal = await Goal.findById(req.params.id)
+
+    if (!goal) {
+        res.status(400);
+        throw new Error("Goal not found");
+    }
+
+    const result = await Goal.findByIdAndDelete(req.params.id);
+
+
+    res.status(200).json({ deleted: req.params.id })
 })
 
 
